e8aa7ef0039287105e79c3052b66f49e2c4230d0
R3gardless <pidaoh@g.skku.edu>
2023-05-14 17:30:26 +0900

    [Fix] CORS origin Bug Fix

--- main.py
+++ main.py
@@ -19,9 +19,13 @@ models.Base.metadata.create_all(bind=engine)
 
 app.mount('/images', StaticFiles(directory="static/images"))
 
+origins = [
+    "http://localhost:3000"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
