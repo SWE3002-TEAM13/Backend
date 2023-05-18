01d1708e91a69abf0194cafcbfe3a9834d23a3bc
R3gardless <pidaoh@g.skku.edu>
2023-05-18 18:50:34 +0900

    [Fix] ChatMessage Websocket Fix

--- router/chat.py
+++ router/chat.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, WebSocket, HTTPException, status
+from fastapi import APIRouter, Depends, WebSocket, HTTPException, status, WebSocketDisconnect
 from auth.oauth2 import get_current_user, verify_token
 from db.database import get_db
 from sqlalchemy.orm.session import Session
@@ -28,8 +28,26 @@ def getChatRoomInfo(chatroom_id: int, db: Session = Depends(get_db), current_use
 
     return db_chat.getChatRoomMessage(chatroom_id, current_user.id, db)
 
-chat_client = []
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
 
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+            
+manager = ConnectionManager()
+            
 @router.websocket("/ws/{chatroom_id}")
 async def sendChatMessage(websocket: WebSocket, chatroom_id: int, token: str | None, db: Session = Depends(get_db)):
     
@@ -49,12 +67,14 @@ async def sendChatMessage(websocket: WebSocket, chatroom_id: int, token: str | N
     
     if valid != "valid": return "Non Valid!"
     
-    await websocket.accept()
-    chat_client.append(websocket)
-    while True:
-        message = await websocket.receive_text()
-        if not message: continue
-        db_chat.createChatMessage(chatroom_id, user.id, message, db)
-
-        for client in chat_client:
-            await client.send_text(message)
+    await manager.connect(websocket)
+    try:
+        while True:
+            message = await websocket.receive_text()
+            if not message: continue
+            db_chat.createChatMessage(chatroom_id, user.id, message, db)
+            await manager.send_personal_message(f'You Wrote : {message}', websocket)
+            await manager.broadcast(f'Client says: {message}')
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client left the chat")
