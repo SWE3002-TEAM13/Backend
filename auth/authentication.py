089e489480f2c174566d0d4e65c7a708b5dcdc9b
R3gardless <pidaoh@g.skku.edu>
2023-05-21 17:00:25 +0900

    [Fix] Login Not user Error message

--- auth/authentication.py
+++ auth/authentication.py
@@ -16,7 +16,7 @@ def login(request:OAuth2PasswordRequestForm = Depends(), db: Session = Depends(g
     user = db.query(models.User).filter(models.User.username == request.username).first()
     if not user:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                                detail=f"Invalid Credentials") 
+                                detail=f"Any User with that ID") 
     if not Hash.verify(user.password, request.password):
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                                 detail=f"Incorrect password") 
